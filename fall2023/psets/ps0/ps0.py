f16a3d38369b6b1e3fa7c87fc7ef2b2b24eb4b0d
paula1284 <112826629+paula1284@users.noreply.github.com>
2023-09-12 15:59:16 -0400

    finished

--- fall2023/psets/ps0/ps0.py
+++ fall2023/psets/ps0/ps0.py
@@ -30,8 +30,11 @@ class BTvertex:
 # ... tree rooted at vertex v to the size of that subtree
 # Runtime: O(n)
 def calculate_sizes(v):
-    # Your code goes here
-    pass
+    if v == None:
+        return 0
+    else:
+        v.size = 1 + calculate_sizes(v.left) + calculate_sizes(v.right)
+    return v.size
 
 #
 # Problem 1c
@@ -43,5 +46,18 @@ def calculate_sizes(v):
 # ... in disjoint trees that all have at most n/2 vertices
 # Runtime: O(h)
 def find_vertex(r): 
-    # Your code goes here
-    pass
+    # Helper function to check if the tree rooted at vertex "child" has size 
+    # less than r.size / 2
+    def check_child(child):
+        if child == None or child.size <= r.size / 2:
+            return True
+        return False
+            
+    # Return a descendent vertex if their size is greater than r.size / 2
+    if not check_child(r.left):
+        return find_vertex(r.left)
+    elif not check_child(r.right):
+        return find_vertex(r.right)
+    
+    # Return parent vertex if neither child has size > r.size / 2
+    return r
